f83de8d6986d2945aaa28df4a8a4ea1abc288c45
jagalite <jagatranvo@gmail.com>
2018-11-20 03:15:12 +0000

    spectrogram learning

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scipy.io import wavfile
 
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import to_categorical
 
 np.random.seed(7)
 
@@ -15,10 +16,26 @@ songsLabel = []
 
 def readSongAndEvents(songPath, eventsPath):
     
-    global songsData
-    song = AudioSegment.from_ogg('Training/Beat it/Beat It.ogg').get_array_of_samples()
-    inputData = np.asarray( song, dtype="int32" )
-    songsData.append(inputData)
+    # global songsData
+    # song = AudioSegment.from_ogg('Training/Beat it/Beat It.ogg').get_array_of_samples()
+    # inputData = np.asarray( song, dtype="int32" )
+    
+    song = AudioSegment.from_ogg("Training/Beat it/Beat It.ogg")
+    song = song.set_channels(1)
+    song.export("Training/Beat it/Beat It.wav", format="wav")
+    sample_rate, samples = wavfile.read('Training/Beat it/Beat It.wav')
+    frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+    timeToSpecMap = {}
+    for i in range(len(times)):
+        if(round(times[i],1) not in timeToSpecMap):
+            timeToSpecMap[round(times[i],1)] = []
+        freq = []
+        for j in range(len(frequencies)):
+            freq.append(spectrogram[j][i])
+        timeToSpecMap[round(times[i],1)].append(freq)
+
+    
+    # songsData.append(frequencies)
     
     with open('Training/Beat it/Easy.json', 'r') as jsonFile:
         
@@ -28,12 +45,22 @@ def readSongAndEvents(songPath, eventsPath):
         
         eventsArray = []
         for event in eventsDict:
-            eventsArray.append(event['_time'])
+            eventsArray.append(round(event['_time'],1))
+        eventsSet = set(eventsArray)
         
-        outputData = np.asarray( eventsArray, dtype="int32" )
-        
+        global songsData
         global songsLabel
-        songsLabel.append(outputData)
+        
+        for time in timeToSpecMap.keys():
+            beat = 0
+            if time in eventsArray:
+                beat = 1
+            for freq in timeToSpecMap[time]:
+                songsData.append(freq)
+                songsLabel.append(beat)
+        
+        #outputData = np.asarray( eventsArray, dtype="int32" )
+        #songsLabel.append(outputData)
         
 readSongAndEvents('Training/Beat it/Beat It.ogg', 'Training/Beat it/Easy.json')
 readSongAndEvents('Training/Beat it/Beat It.ogg', 'Training/Beat it/Medium.json')
@@ -41,13 +68,17 @@ readSongAndEvents('Training/Beat it/Beat It.ogg', 'Training/Beat it/Hard.json')
 readSongAndEvents('Training/Beat it/Beat It.ogg', 'Training/Beat it/Expert.json')
     
 songsData = np.asarray(songsData)
-songsLabel = np.asarray(songsLabel)
 
-print(len(songsData))
-print(len(songsLabel))
+songsLabel = np.asarray(songsLabel)
+songsLabel = to_categorical(songsLabel)
+
+print(songsData)
+print(songsLabel)
 
 model = Sequential()
-model.add(Dense(units=13, activation='relu', input_shape=songsData[0].shape))
-model.add(Dense(units=878, activation='softmax'))
+model.add(Dense(units=10, activation='relu', input_shape=songsData[0].shape))
+model.add(Dense(units=2, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='sgd',metrics=['accuracy'])
 model.fit(songsData, songsLabel, epochs=5, batch_size=10)
+
+
